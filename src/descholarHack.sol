c573fec4e68000fe8d407b7747362ff4f35c88f0
f1l1ph <filipmasarik04@gmail.com>
2025-03-01 21:41:34 +0100

    started working on hackaton implementation

---
+++ src/descholarHack.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract descholarHack {
+    // @Featuers:
+    // hackathon creator
+    // hackathon funder/investor
+    // hackathon judges
+    // hackathon participants
+    // participants can make teams
+}
